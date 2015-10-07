98cc09fd00bb7eb96acbd877e34a6c969b95932f
iluque95 <iludi22@hotmail.es>
2015-10-07 19:27:25 +0200

    From Vilanova

--- Application.java
+++ Application.java
@@ -19,7 +19,7 @@ import static java.lang.System.exit;
 public class Application {
 	public static void main(String[] args) {
 		new A();
-		out.println("Hello World From Java!");
+		out.println("Hello World From Vilanova");
 
 		//Exemple d'us d'atributs estàtics
 		out.println("PI = " + PI);
